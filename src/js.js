5265fad185453ca1ecb66db7f73d2d328c5a8465
SonyaPetkova <90566998+SonyaPetkova@users.noreply.github.com>
2022-04-05 17:43:25 +0200

    Updated the time

--- src/js.js
+++ src/js.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemp(response) {
   console.log(response.data);
   let tempElement = document.querySelector("#temperature");
@@ -8,10 +31,12 @@ function displayTemp(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  let dataElement = document.querySelector("#date");
+  dataElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "34a4206d301517fba52add27cb8eade5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
