4868cacb32d5f2752d3813e6c1e997d52f9a9d9d
SonyaPetkova <90566998+SonyaPetkova@users.noreply.github.com>
2022-04-05 17:21:07 +0200

    Showing current temperature,wind and humidity, Added Open weather API

--- src/js.js
+++ src/js.js
@@ -1,1 +1,18 @@
+function displayTemp(response) {
+  console.log(response.data);
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+}
 
+let apiKey = "34a4206d301517fba52add27cb8eade5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemp);
