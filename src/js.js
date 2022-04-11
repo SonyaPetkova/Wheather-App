fb05f0e1d9e3fdb1d336ecd002875d81fc37738a
SonyaPetkova <90566998+SonyaPetkova@users.noreply.github.com>
2022-04-11 13:11:54 +0200

    Added displayForecast function in JS

--- src/js.js
+++ src/js.js
@@ -21,6 +21,31 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let foreCastElement = document.querySelector("#forecast");
+
+  let foreCastHTML = `<div class="row">`;
+  let days = ["Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    foreCastHTML =
+      foreCastHTML +
+      `
+  <div class="col-2">
+    <div class="weather-forecast-date"> ${day}</div>
+
+<img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="" width="60">
+           <div class="weather-forecast-temperature">
+             <span class="weather-forecast-temperature-max">14°</span> 
+            <span class="weather-forecast-temperature-min">16°</span> </div> 
+  </div>
+`;
+  });
+
+  foreCastHTML = foreCastHTML + `</div>`;
+  foreCastElement.innerHTML = foreCastHTML;
+}
+
 function displayTemp(response) {
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(response.data.main.temp);
@@ -84,3 +109,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 search("Sofia");
+displayForecast();
